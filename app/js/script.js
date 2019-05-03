bdd8f738c8fe28ed6bbd170c0db4a5a7a699cc4d
Redrica <de.spring@gmail.com>
2019-05-03 11:01:30 +0300

    desktop done

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,18 @@
 (function () {
-    console.log('hi!');
-});
+    var form = document.querySelector('.write__form'),
+        name = document.getElementById('name'),
+        email = document.getElementById('email'),
+        phone = document.getElementById('phone');
+
+    console.log(name);
+
+
+    form.addEventListener('submit', function (evt) {
+        if (name.value === '') {
+            evt.preventDefault();
+            console.log('не заполнено имя');
+        }
+
+        console.log(name.value);
+    })
+})();
